e25db268f343b11ec2fa87ab6a7394b0acb847ec
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-07-12 04:51:05 +0000

    arquivos essenciais

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: "#6d6d6f",
+        main_dark: "#3d3d3f",
+        dark: "#2e3845",
+        color1: "#ff656d",
+        color2: "#fbaa72"
+      },
+    },
   },
   plugins: [],
-}
+};
