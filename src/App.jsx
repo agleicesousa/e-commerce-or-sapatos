87c15a346bcf4c821c7588de3020fc8d6469cdb9
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-07-12 07:19:29 +0000

    Add Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    })
+  }, [])
+  
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
