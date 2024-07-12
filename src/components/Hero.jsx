504f4c41c8aaa79fd44630713f8e273116bf07c6
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-07-12 22:18:14 +0000

    Atualização - Hero.jsx e Navbar.jsx = Concluído

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HiPlus } from 'react-icons/hi';
-import img5 from '../assets/img5.png'
-import img4 from '../assets/img4.png'
+import img5 from '../assets/img5.png';
+import img4 from '../assets/img4.png';
 
 export default function Hero() {
   return (
@@ -12,29 +12,29 @@ export default function Hero() {
         </div>
         <div className="h-full flex justify-center items-center w-full overflow-hidden">
           <div className="bg-dark rounded-full shadow relative lg:w-96 w-64 lg:h-96 h-64">
-            <div className="flex flex-col items-center justify-center bg-gradient-to-r from-color1 to-color1 rounded-full p-4 absoluted top-0 z-20">
-              <p className="text-sm text-white">promo: nike</p>
+            <div className="flex flex-col items-center justify-center bg-gradient-to-r from-color2 to-color1 rounded-full p-4 absolute top-0 z-20">
+              <p className="text-sm text-white">promo: Adidas</p>
               <p className="font-bold text-2xl">40%</p>
               <p className="font-bold text-2xl text-white">oferta</p>
             </div>
-            <button className="text-white bg-gradient-to-r from-color2 to color1 rounded-full shadow-2x1 absolute w-10 h-10 top-8 right-5 z-20 flex items-center justify-center">
+            <button className="text-white bg-gradient-to-r from-color2 to-color1 rounded-full shadow-2xl absolute w-10 h-10 top-8 right-5 z-20 flex items-center justify-center">
               <HiPlus size={20} />
             </button>
           </div>
           <img src={img5} alt="" className="rotate-[30deg] lg:w-auto w-4/5 absolute lg:pl-48 lg:pb-32 pb-16 z-10"/>
         </div>
         <div className="lg:absolute lg:w-1/3">
-          <p className="text-gray-500 twxt-xl font-bold">02</p>
+          <p className="text-gray-500 text-xl font-bold">02</p>
           <h2 className="text-5xl text-white font-bold py-8">Sapatos de marca.</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales ligula lorem, accumsan fringilla enim lobortis id. Sed fringilla ex suscipit fermentum vulputate.
+          <p className="text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales ligula lorem, accumsan fringilla enim lobortis id. Sed fringilla ex suscipit fermentum vulputate.
           </p>
         </div>
-        <div className="rounded-full shadow-2x1 bg-gradient-to-r from-color2 to-color1 h-40 w-40 absolute right-[10%] lg:mt-32 lg:flex hidden items-center justify-center">
-          <div className="h-32 w-32-32 rounded-full border border-solid border-gray-200">
+        <div className="rounded-full shadow-2xl bg-gradient-to-r from-color2 to-color1 h-40 w-40 absolute right-[10%] lg:mt-32 lg:flex hidden items-center justify-center">
+          <div className="h-32 w-32 rounded-full border border-solid border-gray-200">
             <img src={img4} alt="" className="w-full h-full m-auto"/>
-            <button className="bg-dark text-white px-2 rounded-lg text-sm font-semibold bottom-10 left-0">R$100.00</button>
-            <button className="text-white bg-gradient-to-r from-color2 to-color1 rounded-full shadow-2x1 absolute w-10 h-10 top-8 right-0 z-10 flex items-center justify-center">
+            <button className="bg-dark text-white px-2 rounded-lg text-sm font-semibold absolute bottom-10 left-0">R$100.00</button>
+            <button className="text-white bg-gradient-to-r from-color2 to-color1 rounded-full shadow-2xl absolute w-10 h-10 top-8 right-0 z-10 flex items-center justify-center">
               <HiPlus size={20} />
             </button>
             <p className="uppercase font-bold absolute top-[4.6rem] -right-16">AIR <span className="text-white">JORDAN</span></p>
@@ -42,5 +42,5 @@ export default function Hero() {
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,22 @@
 import React, { useState } from 'react';
-
-import { HiOutlineShoppingBag, HiMenuAlt3 } from "react-icons/hi"
-import { GoPerson } from "react-icons/go"
-import { MdClose } from "react-icons/md"
+import { HiOutlineShoppingBag, HiMenuAlt3 } from "react-icons/hi";
+import { GoPerson } from "react-icons/go";
+import { MdClose } from "react-icons/md";
 
 export default function Navbar() {
-
-  const [menuSuspenso, setMenuSuspenso] = useState(false)
+  const [menuSuspenso, setMenuSuspenso] = useState(false);
 
   const mostreMenuSuspenso = () => {
-    setMenuSuspenso(!menuSuspenso)
-  }
-  
+    setMenuSuspenso(!menuSuspenso);
+  };
+
   return (
-    <nav className="w-full h-20 flex flex-col justify-center items-center sticky top-0 z-50 bg-gradient-to-r from-color1 to-color2 lg:shadow-2x1">
+    <nav className="w-full h-20 flex flex-col justify-center items-center sticky top-0 z-50 bg-gradient-to-r from-color1 to-color2 lg:shadow-2xl">
       <div className="w-full">
         <div className="lg:w-4/5 w-11/12 mx-auto h-full flex justify-between items-center">
           <div className="flex flex-col gap-y-4">
             <div className="flex items-center gap-x-2">
-              <h2 className="text-withe font-bold text-lg">Tênis</h2>
+              <h2 className="text-white font-bold text-lg">Tênis</h2>
             </div>
           </div>
           <ul className="flex items-center xl:gap-12 gap-x-4 max-lg:hidden">
@@ -30,10 +28,10 @@ export default function Navbar() {
             <a href="#" className="leading-normal no-underline text-white text-lg hover:text-dark">Suporte</a>
           </ul>
           <div className="flex gap-4 max-lg:hidden">
-            <button className="bg-dark rounded-full shadow h-10 w-10 outline-none text-white hover:bg-white hover:text-dark cursor-ponter text-base/6">
+            <button className="bg-dark rounded-full shadow h-10 w-10 outline-none text-white hover:bg-white hover:text-dark cursor-pointer text-base transition-bg hover:border hover:border-dark">
               <HiOutlineShoppingBag className="w-full h-full p-2"/>
             </button>
-            <button className="bg-dark rounded-full shadow h-10 w-10 outline-none text-white hover:bg-white hover:text-dark cursor-ponter text-base/6">
+            <button className="bg-dark rounded-full shadow h-10 w-10 outline-none text-white hover:bg-white hover:text-dark cursor-pointer text-base transition-bg hover:border hover:border-dark">
               <GoPerson className="w-full h-full p-2"/>
             </button>
           </div>
@@ -63,5 +61,5 @@ export default function Navbar() {
         )}
       </div>
     </nav>
-  )
+  );
 }
