7474d1e0fe16dce04284d32d49af0cf65b546d84
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-07-14 04:16:43 +0000

    Atualização - v3.0 - pendente

--- src/mocks/listaSapatos.js
+++ src/mocks/listaSapatos.js
@@ -1,105 +1,123 @@
- const sapatos = [
+import img1 from '../assets/img1.png';
+import img2 from '../assets/img2.png';
+import img3 from '../assets/img3.png';
+import img4 from '../assets/img4.png';
+import img5 from '../assets/img5.png';
+import img6 from '../assets/img6.png';
+import img7 from '../assets/img7.png';
+import img8 from '../assets/img8.png';
+import img9 from '../assets/img9.png';
+import img10 from '../assets/img10.png';
+import img11 from '../assets/img11.png';
+import img12 from '../assets/img12.png';
+import img13 from '../assets/img13.png';
+import img14 from '../assets/img14.png';
+import img15 from '../assets/img15.png';
+import img16 from '../assets/img16.png';
+import img17 from '../assets/img17.png';
+
+const sapatos = [
     {
         id: 1,
         nome: 'NIKE WMNS ZOOM AIR FIRE WHITE/UNIVERSITY RED',
         preco: 'R$ 599,99',
-        img: '../assets/img1.png',
+        img: img1,
     },
     {
         id: 2,
         nome: 'ADIDAS ULTRABOOST 21',
         preco: 'R$ 749,99',
-        img: '../assets/img2.png',
+        img: img2,
     },
     {
         id: 3,
         nome: 'PUMA RS-X3 PUZZLE',
         preco: 'R$ 529,99',
-        img: '../assets/img3.png',
+        img: img3,
     },
     {
         id: 4,
         nome: 'REEBOK NANO X1',
         preco: 'R$ 699,99',
-        img: '../assets/img4.png',
+        img: img4,
     },
     {
         id: 5,
         nome: 'ASICS GEL-KAYANO 27',
         preco: 'R$ 649,99',
-        img: '../assets/img5.png',
+        img: img5,
     },
     {
         id: 6,
         nome: 'NEW BALANCE 990V5',
         preco: 'R$ 799,99',
-        img: '../assets/img6.png',
+        img: img6,
     },
     {
         id: 7,
         nome: 'MIZUNO WAVE PROPHECY X',
         preco: 'R$ 899,99',
-        img: '../assets/img7.png',
+        img: img7,
     },
     {
         id: 8,
         nome: 'UNDER ARMOUR HOVR PHANTOM 2',
         preco: 'R$ 599,99',
-        img: '../assets/img8.png',
+        img: img8,
     },
     {
         id: 9,
         nome: 'OAKLEY CHOPROCK',
         preco: 'R$ 479,99',
-        img: '../assets/img9.png',
+        img: img9,
     },
     {
         id: 10,
         nome: 'FILA DISRUPTOR II',
         preco: 'R$ 399,99',
-        img: '../assets/img10.png',
+        img: img10,
     },
     {
         id: 11,
         nome: 'CONVERSE ALL STAR CHUCK TAYLOR',
         preco: 'R$ 299,99',
-        img: '../assets/img11.png',
+        img: img11,
     },
     {
         id: 12,
         nome: 'VANS OLD SKOOL',
         preco: 'R$ 349,99',
-        img: '../assets/img12.png',
+        img: img12,
     },
     {
         id: 13,
         nome: 'JORDAN AIR JORDAN 1 MID',
         preco: 'R$ 799,99',
-        img: '../assets/img13.png',
+        img: img13,
     },
     {
         id: 14,
         nome: 'DIADORA TITAN II',
         preco: 'R$ 459,99',
-        img: '../assets/img14.png',
+        img: img14,
     },
     {
         id: 15,
         nome: 'SKECHERS DLITES',
         preco: 'R$ 379,99',
-        img: '../assets/img15.png',
+        img: img15,
     },
     {
         id: 16,
         nome: 'LE COQ SPORTIF COURTCLASSIC',
         preco: 'R$ 329,99',
-        img: '../assets/img16.png',
+        img: img16,
     },
     {
         id: 17,
         nome: 'KAPPA INSPIRED',
         preco: 'R$ 289,99',
-        img: '../assets/img17.png',
+        img: img17,
     },
 ];
 
